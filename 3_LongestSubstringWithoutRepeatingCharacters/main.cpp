f0d3cbfa7c084d72f07bcd5cecfde801c1209bff
maxyrepo <maxy218@debian.maxy218_debian.com>
2017-03-22 08:23:05 +0800

    problem 3 accepted

---
+++ 3_LongestSubstringWithoutRepeatingCharacters/main.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int char_idx[256];
+        for(size_t i = 0; i < 256; ++i) {
+            char_idx[i] = -1;
+        }
+    
+        int cur_max_len = 0;
+        int global_max_len = 0;
+        for(size_t i = 0; i < s.size(); ++i) {
+            int pos_diff = i - char_idx[s[i]];
+            cur_max_len = pos_diff < cur_max_len + 1 ? pos_diff : cur_max_len + 1;
+            if (cur_max_len > global_max_len) {
+                global_max_len = cur_max_len;
+            }
+            char_idx[s[i]] = i;
+        }
+        return global_max_len;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << "abcabcbb: " << s.lengthOfLongestSubstring("abcabcbb") << endl;
+    cout << "bbbbb: " << s.lengthOfLongestSubstring("bbbbb") << endl;
+    cout << "pwwkew: " << s.lengthOfLongestSubstring("pwwkew") << endl;
+    cout << "a: " << s.lengthOfLongestSubstring("a") << endl;
+    cout << ": " << s.lengthOfLongestSubstring("") << endl;
+}
